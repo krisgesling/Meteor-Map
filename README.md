c05d6ba93d328b02cb43127a171d5f96a6190d8e
Kris Gesling <krisgesling@gmail.com>
2016-07-23 08:59:00 +0930

    Fix css linting errors

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Meteor Strike Map
 A map of the earth showing all known meteor strikes throughout history. Limited data on each meteor is available by hovering over any of the strikes.
 
-This is a project for the FreeCodeCamp Data Visualisation Certificate. FreeCodeCamp.com is an open source and welcoming community of people learning to code. Great for those who have never touched HTML through to intermediate programmers. Check it out.
+This is a project for the FreeCodeCamp Data Visualisation Certificate. [FreeCodeCamp.com](https://www.freecodecamp.com/) is an open source and welcoming community of people learning to code. Great for those who have never touched HTML through to intermediate programmers. Check it out.
 
 > Objective: Build a CodePen.io app that is functionally similar to this: http://codepen.io/FreeCodeCamp/full/mVEJag.
 >
--- js/meteor-map.js
+++ js/meteor-map.js
@@ -1,48 +1,72 @@
+/*** CONFIG ***/
+var mapID = '#map-container';
+
 // Map dimensions
 var w = window.innerWidth;
 var h = window.innerHeight;
-var rScale;
 
 var worldJSON = 'data/world-geo2-min.json';
-
 var dataJSON = 'data/meteorite-strike-data.json';
+var rScale;
 
 function scaleCalc(data) {
     //console.log('ping');
   //console.log(data[1]);
-
   var rMax = d3.max(data, function(d) {
-    //console.log(d);
-    return d.properties.mass;
+    return d.properties.mass ? d.properties.mass : 0;
   });
-    rScale = d3.scale.log()
-    .domain([0,rMax])
-    .range([1,10]);
+
+  rScale = d3.scale.log()
+                .domain([0,rMax])
+                .range([1,10]);
+
+}
+
+function sizeChange() {
+  d3.select('#map-svg')
+    .attr({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
 }
 
 function renderMap(world, data) {
 
+
+  function zoomed() {
+        svg.attr('transform','translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
+      }
+  var scaleMod = window.innerwidth < 900 ? 20 : 180;
   var projection = d3.geo.miller()
       .translate([w/2,h/2])
-      .scale(180);
+      .scale(scaleMod);
   var path = d3.geo.path()
                    .projection(projection);
 
-  var svg = d3.select('body')
+  var zoom = d3.behavior.zoom()
+                   .scale(1)
+                   .scaleExtent([1,12])
+                   .on('zoom', zoomed);
+
+  var svg = d3.select(mapID)
 					  	.append('svg')
-					  	.attr('width', w)
-					  	.attr('height', h)
-              .style('background','#eee');/*
-              THIS IS FOR ZOOM
-  .append("g")
-    .call(d3.behavior.zoom().scaleExtent([1, 8]).on("zoom", zoom))
-  .append("g");
+					  	.attr({
+                width: w,
+					  	  height: h,
+                id: 'map-svg'
+              })
+              .style('background','#BECFE1')
+              .append('g')
+              .attr('id', 'firstG')
+              .call(zoom)
+              .append('g')
+              .attr('id', 'secondG');
 
   svg.append("rect")
     .attr("class", "overlay")
     .attr("width", w)
     .attr("height", h)
-  .style('background','#266D98');*/
+  .style('background','#266D98');
 
   d3.json(world, function(json) {
         svg.selectAll('path')
@@ -50,12 +74,15 @@ function renderMap(world, data) {
            .enter()
            .append('path')
            .attr('d', path)
+           .attr('stroke', '#ccc')
+           .attr('stroke-width', '0.2px')
            .attr('id', function(d) {
           return d.id;
         })
            .style('fill', '#004500');
 });
   d3.json(data, function(json) {
+
     svg.selectAll('circle')
        .data(json.features)
        .enter()
@@ -67,22 +94,14 @@ function renderMap(world, data) {
          if (d.geometry) { return projection([d.geometry.coordinates[0],d.geometry.coordinates[1]])[1]; }
        })
       .attr('r', function(d) {
-        return d.properties.mass/1000000+1;
+        //return d.properties.mass/1000000+1;
         // need to shift to using scale
-        // rScale(d.properties.mass);
+        return d.properties.mass ? rScale(d.properties.mass) : 1;
       })
       .style('fill', function (d) {
-          return (d.properties.id ==  23593) ? 'red' : 'yellow';
+          return (d.properties.id ==  23593) ? '#6F1C2C' : 'yellow';
     });
   });
-}
-
-function addDataPoints(data) {
-
-}
-/*** Zooming ***/
-function zoom() {
-  svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
 }
 
 /*** Primary calls ***/
@@ -92,4 +111,4 @@ d3.json(dataJSON, function(error, json) {
 });
 renderMap(worldJSON, dataJSON);
 
-
+d3.select(window).on("resize", sizeChange);
